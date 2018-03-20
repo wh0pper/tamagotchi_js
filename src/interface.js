b969c44bf85244d984b90c4175f3d9d494abd136
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-03-20 10:41:09 -0700

    separate creation and play, add styles

---
+++ src/interface.js
@@ -0,0 +1,57 @@
+import {tamagotchi} from './tamagotchi';
+import $ from 'jquery';
+
+export class Interface {
+  // static genesis() {
+    // var createButton = document.createElement("button");
+    // createButton.innerHTML = "Create";
+    // document.getElementsByClassName('container')[0].appendChild(createButton);
+    // createButton.addEventListener('click', function() {
+    //   Interface.play();
+    // });
+  //
+  // }
+
+  static play() {
+    $(".creation").hide();
+    $(".playing").show();
+
+    tamagotchi.setHunger();
+    tamagotchi.setFun();
+    tamagotchi.setHygiene();
+    tamagotchi.setAge();
+
+    $("button#feed").click(function() {
+      tamagotchi.feed();
+    });
+
+
+    var game = setInterval(playing, 1000);
+
+    function playing() {
+      $("#food").text(`Food level: ${tamagotchi.foodLevel}/10`);
+      $("#fun").text(`Fun level: ${tamagotchi.funLevel}/10`);
+      $("#hygiene").text(`Cleanliness: ${tamagotchi.hygieneLevel}/10`);
+
+      //age
+      if (tamagotchi.age >= 0 && tamagotchi.age < 2) {
+        document.getElementById("output").innerHTML = 'egg'; //"<img src='img/egg.png'>";
+      } else if (tamagotchi.age >= 2 && tamagotchi.age < 4) {
+        document.getElementById("output").innerHTML = 'hatch'; //"<img src='img/hatch.png'>";
+      } else if (tamagotchi.age >= 4 && tamagotchi.age < 25) {
+        document.getElementById("output").innerHTML = 'happy'; //"<img src='img/happy.png'>";
+      } else if (tamagotchi.age == 25) {
+        document.getElementById("output").innerHTML = 'dead'; //"<img src='img/dead.png'>";
+      }
+
+      //levels
+      if (tamagotchi.foodLevel < 3) {
+        document.getElementById("output").innerHTML = 'hungry'; //"<img src='img/hungry.png'>";
+      }
+      if (tamagotchi.funLevel < 3) {
+        document.getElementById("output").innerHTML = 'frustrated'; //"<img src='img/frustrated.png'>";
+      }
+
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,56 +1,20 @@
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
 import $ from 'jquery';
 import {tamagotchi} from './tamagotchi';
+import { Interface } from './interface';
 
-// $(document).ready(function() {
-  var createButton = document.createElement("button");
-  createButton.innerHTML = "Create";
-  document.getElementsByClassName('container')[0].appendChild(createButton);
-
-  $("button#feed").click(function() {
-    tamagotchi.feed();
+$(document).ready(function() {
+  $('form#creation_form').submit(function(event) {
+    event.preventDefault();
+    let name = $('input#name').val();
+    tamagotchi.name = name;
+    Interface.play();
   });
+});
 
-
+// let promise = new Promise(function(resolve, reject) {
+//
 // })
-
-tamagotchi.setHunger();
-tamagotchi.setFun();
-tamagotchi.setHygiene();
-tamagotchi.setAge();
-
-// var game = setInterval(playing, 1000);
-
-function playing() {
-  $("#food").text(`Food level: ${tamagotchi.foodLevel}/10`);
-  $("#fun").text(`Fun level: ${tamagotchi.funLevel}/10`);
-  $("#hygiene").text(`Cleanliness: ${tamagotchi.hygieneLevel}/10`);
-
-  // console.log(tamagotchi.age);
-  if (tamagotchi.age >= 0 && tamagotchi.age < 2) {
-    document.getElementById("output").innerHTML = "<img src='img/egg.png'>";
-  }
-
-  if (tamagotchi.age >= 2 && tamagotchi.age < 4) {
-    document.getElementById("output").innerHTML = "<img src='img/hatch.png'>";
-  }
-
-  if (tamagotchi.age >= 4 && tamagotchi.age < 25) {
-    document.getElementById("output").innerHTML = "<img src='img/happy.png'>";
-  }
-
-  if (tamagotchi.age == 25) {
-    document.getElementById("output").innerHTML = "<img src='img/dead.png'>";
-  }
-
-  if (tamagotchi.foodLevel < 3) {
-    document.getElementById("output").innerHTML = "<img src='img/hungry.png'>";
-  }
-
-  if (tamagotchi.funLevel < 3) {
-    document.getElementById("output").innerHTML = "<img src='img/frustrated.png'>";
-  }
-
-
-}
+//
